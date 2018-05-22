a6018be64cf514e54490c6dc8b6ebdb9476bd0fb
sanny <sannysoft@gmail.com>
2018-05-22 17:39:57 +0300

    Update to proper field names v2

--- src/main/java/com/checkout/api/services/charge/response/Charge.java
+++ src/main/java/com/checkout/api/services/charge/response/Charge.java
@@ -2,6 +2,7 @@ package com.checkout.api.services.charge.response;
 
 import com.checkout.api.services.card.response.Card;
 import com.checkout.api.services.charge.request.BaseCharge;
+import com.sun.istack.internal.Nullable;
 
 public class Charge extends BaseCharge {
     public String id;
@@ -15,13 +16,27 @@ public class Charge extends BaseCharge {
     public boolean isCascaded;
     public String authCode;
 
+    @Nullable
+    public String redirectUrl;
+
     /*
      * 3D Secure charge verification specific fields
      */
+    @Nullable
     public String enrolled;
+
+    @Nullable
     public String signatureValid;
+
+    @Nullable
     public String authenticationResponse;
+
+    @Nullable
     public String eci;
+
+    @Nullable
     public String cavv;
+
+    @Nullable
     public String xid;
 }

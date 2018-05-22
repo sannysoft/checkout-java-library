72ff806ca636494b6d0cf1a46b3c710dd0a339de
sanny <sannysoft@gmail.com>
2018-05-22 17:02:39 +0300

    Update to proper field names

--- src/main/java/com/checkout/api/services/charge/request/BaseCharge.java
+++ src/main/java/com/checkout/api/services/charge/request/BaseCharge.java
@@ -5,35 +5,50 @@ import com.checkout.api.services.shared.Product;
 
 import java.util.List;
 
+@SuppressWarnings("WeakerAccess")
 public class BaseCharge extends BaseChargeInfo {
 
-    public static final String YES = "y";
-    public static final String NO = "n";
+    public static final String YES = "Y";
+    public static final String NO = "N";
 
-    public static final int THREEDS = 2;
-    public static final int NON_THREEDS = 1;
+    public static final int THREEDS_YES = 2;
+    public static final int THREEDS_NO = 1;
+
+    public static final String TYPE_REGULAR = "1";
+    public static final String TYPE_RECURRING = "2";
+    public static final String TYPE_MOTO = "3";
 
     public String value;
     public String currency;
-    public int chargeMode;
-    public String autoCapture;
+
+    public int chargeMode = THREEDS_NO;
+
+    public String autoCapture = YES;
     public int autoCapTime;
-    public String customerIp;
+
     public String email;
+    public String customerIp;
     public String customerId;
     public String customerName;
+
     public List<Product> products;
+
     public Address shippingDetails;
-    public String redirectUrl;
+    public Address billingDetails;
+
+    public String successUrl;
+    public String failUrl;
+
     public BillingDescriptor descriptor;
+
     public boolean attemptN3D;
     public boolean riskCheck;
-    public String transactionIndicator;
+
+    public String transactionIndicator = TYPE_REGULAR;
+
     public boolean cardOnFile;
     public String previousChargeId;
 
     public BaseCharge() {
-        autoCapture = YES;
-        chargeMode = NON_THREEDS; //Default mode is no 3D
     }
 }

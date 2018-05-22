9e94af0ea6349be4beae75693c2bc487b26178bb
sanny <sannysoft@gmail.com>
2018-05-22 18:02:51 +0300

    fix nullable

--- src/main/java/com/checkout/api/services/charge/response/Charge.java
+++ src/main/java/com/checkout/api/services/charge/response/Charge.java
@@ -2,7 +2,6 @@ package com.checkout.api.services.charge.response;
 
 import com.checkout.api.services.card.response.Card;
 import com.checkout.api.services.charge.request.BaseCharge;
-import com.sun.istack.internal.Nullable;
 
 public class Charge extends BaseCharge {
     public String id;
@@ -16,27 +15,15 @@ public class Charge extends BaseCharge {
     public boolean isCascaded;
     public String authCode;
 
-    @Nullable
     public String redirectUrl;
 
     /*
      * 3D Secure charge verification specific fields
      */
-    @Nullable
     public String enrolled;
-
-    @Nullable
     public String signatureValid;
-
-    @Nullable
     public String authenticationResponse;
-
-    @Nullable
     public String eci;
-
-    @Nullable
     public String cavv;
-
-    @Nullable
     public String xid;
 }
